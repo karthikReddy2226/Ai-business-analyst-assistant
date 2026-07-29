aeb42383ac7bd4f06d943891b1c3f1df0726b0ac
karthikReddy2226 <164987289+karthikReddy2226@users.noreply.github.com>
2026-07-29 19:11:59 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -44,7 +44,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                   "https://ai-business-analyst-assistant-beryl.vercel.app/",
+                   "https://ai-business-analyst-assistant-beryl.vercel.app",
                    ],
     allow_credentials=True,
     allow_methods=["*"],
